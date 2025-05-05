cdf052aad5076669bf9a1d5ff6e658c51faf5701
Sergio <pitufo_bern@hotmail.com>
2025-05-05 14:45:39 -0300

    cambios testr

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -1,16 +1,16 @@
 const FizzBuzz = require('./FizzBuzz');
 
 describe('FizzBuzz', () => {
-  let testPassed = false; // Variable para controlar si el test pasó
+  let testPassed = false; 
 
   test('should print 1 if they receive 1', () => {
     const expected = 1;
-    const result = FizzBuzz(1); // Llamamos a la función FizzBuzz con 1
-    expect(result).toBe(expected); // Verificamos si el resultado es 1
-    testPassed = true; // Si la prueba pasa, cambiamos testPassed a true
+    const result = FizzBuzz(1);
+    expect(result).toBe(expected); 
+    testPassed = true; 
   });
 
-  // Después de todos los tests, verificamos si la prueba pasó
+
   afterAll(() => {
     if (testPassed) {
       console.log('🎉 ¡Felicitaciones alumno de prodevs! El ejercicio fue resuelto correctamente, vas mejorando.');
